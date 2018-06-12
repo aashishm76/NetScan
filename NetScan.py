fff037943df1ef425facff6de245d204255816bb
Aashish <aashishm97@knights.ucf.edu>
2018-06-11 23:58:52 -0400

    Map network base functionality added. More tweaks to improve speed will come in the future.

--- NetScan.py
+++ NetScan.py
@@ -3,7 +3,7 @@
 
 # Library Imports
 import socket
-import sys
+import os
 
 print "NetSCAN 1.0 \n"
 
@@ -19,7 +19,8 @@ def menu():
     print "1. Device Information"
     print "2. Get IP Address of Host"
     print "3. Port Scanner"
-    print "3. Exit \n"
+    print "4. Ping all devices on network"
+    print "5. Exit \n"
 
     choice = input('Enter command: ')
 
@@ -30,12 +31,27 @@ def menu():
     if choice == 3:
         portChecker()
     if choice == 4:
+        currNetwork()
+    if choice == 5:
         exit()
     else:
         print " "
         print "Invalid selection. \n"
         menu()
 
+# Helper request user for address entry
+def addressgetter():
+
+    addressIP = raw_input('Enter an web address: ')
+    return addressIP
+
+# Helper to process device info
+def deviceInfoHelper():
+
+    userHostName = socket.gethostname()
+    userIP = socket.gethostbyname(userHostName)
+
+    return userIP
 
 # Provides device information
 def deviceInfo():
@@ -46,17 +62,16 @@ def deviceInfo():
         print "Host IP: ", userIP
         print " "
         menu()
-    except:
+    except socket.error:
         print 'Unable to resolve hostname'
         menu()
 
 
 # Gets IP address given hostname
 def nameToIP():
-    addressQuery = raw_input('Enter a web address: ')
 
     try:
-        host = socket.gethostbyname(addressQuery)
+        host = socket.gethostbyname(addressgetter())
         print "IP Address: " + host
         print " "
         menu()
@@ -64,11 +79,11 @@ def nameToIP():
         print "\nEnter valid hostname.\n"
         menu()
 
+# Scans for open ports given a specific address
 def portChecker():
 
     # Get the address we want to check the ports of
-    addressQuery = raw_input('Enter a web address: ')
-    addressIP = socket.gethostbyname(addressQuery)
+    addressIP = socket.gethostbyname(addressgetter())
 
     # Scan ports 1 to 1024
     try:
@@ -92,6 +107,25 @@ def portChecker():
     except socket.error, e:
         print "Socket error. Unable to complete operation."
         menu()
+
+# List of all devices on current network
+def currNetwork():
+
+    # Get address of current device to determine what IP Address range to use
+    addressIP = deviceInfoHelper()
+    print('Your Device IP: ' + addressIP)
+
+    # Splice the IP Address to obtain proper range
+    baseIPAddress = addressIP[:11]
+    print('Base IP Address is: ' + baseIPAddress)
+
+    # Iterate through possible IP address and ping them
+    for i in range (1, 200):
+        PingIP = baseIPAddress + str(i)
+        os.system('ping ' + PingIP)
+
+
+
 
 if __name__ == '__main__':
     main()
